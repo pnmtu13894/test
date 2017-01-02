253e01fbd4783d3bfbbd988470210e9961505e7d
minhtu <pnmtu13894@gmail.com>
2017-01-03 01:32:52 +0700

    sadfsadf

--- server/models/books.js
+++ server/models/books.js
@@ -1,33 +1,33 @@
-var mongoose = require('mongoose');
-
-var Book = mongoose.model('Book', {
-  title: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  author: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  type: {
-    type: String,
-    trim: true,
-    minlength: 1
-  },
-  price: {
-    type: Number
-  },
-  time: {
-    type: Date,
-    default: Date.now
-  },
-  image: {
-    type: String
-  }
-});
-
-module.exports = {Book};
+// var mongoose = require('mongoose');
+//
+// var Book = mongoose.model('Book', {
+//   title: {
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   },
+//   author: {
+//     type: String,
+//     required: true,
+//     minlength: 1,
+//     trim: true
+//   },
+//   type: {
+//     type: String,
+//     trim: true,
+//     minlength: 1
+//   },
+//   price: {
+//     type: Number
+//   },
+//   time: {
+//     type: Date,
+//     default: Date.now
+//   },
+//   image: {
+//     type: String
+//   }
+// });
+//
+// module.exports = {Book};
